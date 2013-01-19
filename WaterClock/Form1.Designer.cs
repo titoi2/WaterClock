6d8a26e7b4b7526da7579dbf4f4a02899a77e92f
titoi2 <aboutsoft.jp@gmail.com>
2013-01-19 22:46:14 +0900

    VS hackathon 2013.1.19 in OSAKA
    WaterClock team gitton

---
+++ WaterClock/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WaterClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(482, 653);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.dateTimePicker1.Font = new System.Drawing.Font("MS UI Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(347, 617);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(135, 34);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.trackBar1.Location = new System.Drawing.Point(0, 597);
+            this.trackBar1.Maximum = 3600;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(229, 56);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(238, 606);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 40);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "x1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 653);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "ジトンの水時計";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WaterClock/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WaterClock
+{
+    public partial class Form1 : Form
+    {
+        private Graphics gGraphics;
+        private Pen gPen;
+        private SolidBrush gBrush;
+
+        static readonly int HourBarX = 0;
+        static readonly int HourBarWidth = 72;
+
+        static readonly int MinuteBarX = 158;// 206;
+        static readonly int MinuteBarWidth = 96;
+
+        static readonly int SecondBarX = 258;
+        static readonly int SecondBarWidth = 96;
+
+
+        static readonly int RightPipeX = 158;
+        static readonly int RightPipeY = 40;
+        static readonly int RightPipeWidth = 156;
+        static readonly int RightPipeHeight = 33;
+
+        static readonly int LeftPipeX = 8;
+        static readonly int LeftPipeY = 40;
+        static readonly int LeftPipeWidth = 156;
+        static readonly int LeftPipeHeight = 33;
+
+        static readonly int BASE_WIDTH = 360;
+        static readonly int BASE_HEIGHT = 640;
+
+
+        static readonly int BAR_Y_OFFSET = 74;
+        static readonly int BAR_HEIGHT = 466;
+
+        static readonly int PENDULUM_X = 160;
+        static readonly int PENDULUM_Y = 40;
+        static readonly int PENDULUM_POINT_X = 32;
+        static readonly int PENDULUM_POINT_Y = 30;
+
+        
+        private int gPendulumWidth;
+        private int gPendulumHeight ;
+            
+        private float xaspect;
+        private float yaspect;
+        private int TimeTimes;
+        private float gHour;
+        private float gMinute;
+        private float gSecond;
+        private float gMilSecond;
+
+        Image gImage = null;
+        private Image gPendulumImage;
+        private float gAngle;
+        private float gAngleDif;
+
+        public Form1()
+        {
+            InitializeComponent();
+            TimeTimes = 1;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            long tick = DateTime.Now.Ticks ;
+            tick /= 10000000;
+            tick *= TimeTimes;
+            gSecond = (tick % 60);
+            tick /= 60;
+            gMinute = (tick % 60);
+            tick /= 60;
+            gHour = (tick % 12);
+            gMilSecond = (gSecond * 1000 + DateTime.Now.Millisecond) * TimeTimes % (60*1000);
+
+            dateTimePicker1.Value = DateTime.Now;
+            pictureBox1.Invalidate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+            pictureBox1.Image = bmp;
+            gGraphics = Graphics.FromImage(pictureBox1.Image);
+            gPen = new Pen(Color.Blue);
+            gBrush = new SolidBrush(Color.Cyan);
+
+
+            gImage = new Bitmap(GetType(), "timer_image.png");
+            gPendulumImage = new Bitmap(GetType(), "huriko.png"); 
+
+            gPendulumWidth = gPendulumImage.Width;
+            gPendulumHeight = gPendulumImage.Height;
+
+            long tick = DateTime.Now.Ticks;
+            tick /= 10000000;
+            tick *= TimeTimes;
+            gSecond = (tick % 60);
+            tick /= 60;
+            gMinute = (tick % 60);
+            tick /= 60;
+            gHour = (tick % 12);
+            gMilSecond = (gSecond * 1000 + DateTime.Now.Millisecond) * TimeTimes % (60 * 1000);
+
+            gAngle = 0;
+            gAngleDif = 5;
+            dateTimePicker1.Value = DateTime.Now;
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            gGraphics.Clear(Color.Black);
+            xaspect = (float)pictureBox1.Size.Width / BASE_WIDTH;
+            yaspect = (float)pictureBox1.Size.Height / BASE_HEIGHT;
+
+            gGraphics.ResetTransform();
+
+            Rectangle hourrect = CalcRect(HourBarX, HourBarWidth, gHour + gMinute /60, 12);
+            gGraphics.FillRectangle(gBrush, hourrect);
+            Rectangle minrect = CalcRect(MinuteBarX, MinuteBarWidth, gMinute + gMilSecond / 60000, 60);
+            gGraphics.FillRectangle(gBrush, minrect);
+            Rectangle secondrect = CalcRect(SecondBarX, SecondBarWidth, gMilSecond, 60 * 1000 - 1);
+            gGraphics.FillRectangle(gBrush, secondrect);
+
+            if (gAngle > 13)
+            {
+                Rectangle rpiperect = new Rectangle();
+                rpiperect.X = (int)(RightPipeX * xaspect);
+                rpiperect.Y = (int)(RightPipeY * yaspect);
+                rpiperect.Width = (int)(RightPipeWidth * xaspect);
+                rpiperect.Height = (int)(RightPipeHeight * yaspect);
+                gGraphics.FillRectangle(gBrush, rpiperect);
+            }
+            if (gAngle < -13)
+            {
+                Rectangle lpiperect = new Rectangle();
+                lpiperect.X = (int)(LeftPipeX * xaspect);
+                lpiperect.Y = (int)(LeftPipeY * yaspect);
+                lpiperect.Width = (int)(LeftPipeWidth * xaspect);
+                lpiperect.Height = (int)(LeftPipeHeight * yaspect);
+                gGraphics.FillRectangle(gBrush, lpiperect);
+            }
+
+            gGraphics.DrawImage(gImage, 0, 0, pictureBox1.Size.Width, pictureBox1.Size.Height);
+
+            float pendwidth = gPendulumWidth * pictureBox1.Size.Width / BASE_WIDTH;
+            gGraphics.TranslateTransform((PENDULUM_X * xaspect - pendwidth / 2)
+                , (PENDULUM_Y * yaspect));
+            gGraphics.RotateTransform(gAngle);
+            gAngle += gAngleDif;
+            if (Math.Abs( gAngle) > 15)
+            {
+                gAngleDif = -gAngleDif;
+            }
+            // 振り子描画
+            gGraphics.DrawImage(gPendulumImage, -pendwidth / 2, -PENDULUM_POINT_Y * yaspect,
+              gPendulumWidth *  pictureBox1.Size.Width / BASE_WIDTH,
+              gPendulumHeight *  pictureBox1.Size.Height/ BASE_HEIGHT);
+
+        }
+
+        private Rectangle CalcRect(int x, int width, float n, int max)
+        {
+            Rectangle rect = new Rectangle();
+            rect.X = (int)(x * xaspect);
+            rect.Width = (int)(width * xaspect);
+            float dy = (float)n/max;
+            rect.Y = (int)((BAR_Y_OFFSET + BAR_HEIGHT * (1 - dy)) * yaspect);
+            rect.Height = (int)((BAR_HEIGHT+100) * dy * yaspect);
+            return rect;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (pictureBox1.Size.Width == 0 || pictureBox1.Size.Height == 0)
+            {
+                return;
+            }
+            Bitmap bmp = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+            pictureBox1.Image = bmp;
+            gGraphics = Graphics.FromImage(pictureBox1.Image);
+            pictureBox1.Invalidate();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label1.Text = "x" + trackBar1.Value;
+            TimeTimes = trackBar1.Value;
+        }
+    }
+}
